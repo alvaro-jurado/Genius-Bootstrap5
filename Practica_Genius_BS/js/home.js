e601a9ec2a5ca4bab149c445d63eab630be36628
Alvaro Jurado Cano <100410771+alvaro-jurado@users.noreply.github.com>
2023-11-09 16:21:25 +0100

    Delete Practica_Genius_BS directory

--- Practica_Genius_BS/js/home.js
+++
@@ -1,6 +0,0 @@
-function darkMode() {
-    let bodyDK = document.body;
-    let chartsDK = document.getElementById("charts")
-    bodyDK.classList.toggle("dark-mode");
-    chartsDK.classList.toggle("dark-mode");
- }
