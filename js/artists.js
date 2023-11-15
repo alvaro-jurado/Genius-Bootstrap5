e973c8a6580f523399b8f71370262a18e5e43a2b
Alvaro Jurado <alvarojurcan@campus.monlau.com>
2023-11-15 18:11:02 +0100

    artists searchBar working

--- js/artists.js
+++ js/artists.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", async function () {
     try {
-        const response = await fetch('../json/songsData.json');
+        const response = await fetch('../json/artists.json');
         const data = await response.json();
-        const songs = data;
+        const artists = data;
 
         const playlist = document.createElement('div');
         playlist.classList.add('row');
@@ -13,22 +13,50 @@ document.addEventListener("DOMContentLoaded", async function () {
 
         let index = 0;
 
-        songs.forEach((song) => {
+        artists.forEach((artist) => {
             const listItem = document.createElement('div');
-            listItem.className = "songsList";
+            listItem.className = "artistsList";
             listItem.classList.add('col-3');
+            listItem.classList.add('card');
+            listItem.classList.add('flip-z');
             listItem.innerHTML = `
-                <h1>${song.artist}</h1>
+               <div class="card-front"> <h1>${artist.artist}</h1> </div>
+               <div class="card-back"> <img src="${artist.img}"/> </div>
             `;
             /*listItem.addEventListener('click', () => {
-                img.src = songs[currentIndex].img;
-                artist.innerHTML = songs[currentIndex].artist;
+                img.src = artists[currentIndex].img;
+                artist.innerHTML = artists[currentIndex].artist;
             });*/
             playlist.appendChild(listItem);
             index++;
         });
+        console.log(playlist);
     } catch (error) {
         console.error('Error al cargar el archivo JSON:', error);
     }
+
+
+    let input;
+    input = document.getElementById("search-bar-input");
+
+    function searchBar() {
+        let filter, pl, li, div, i, txtValue;
+
+        filter = input.value.toUpperCase();
+        pl = document.getElementById("playlist");
+        li = pl.getElementsByClassName("artistsList");
+        
+        for (i = 0; i < li.length; i++) {
+            div = li[i].getElementsByTagName("div")[0];
+            txtValue = div.textContent || div.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
+        }
+    }
+
+    input.addEventListener("keyup", searchBar);
 
 });
